ab5ff5bbfde6a84c7dfc8ea4cfbebf49de6b22e9
Joe Pfahl <joegravity@Joe-Pfahls-Mac-Pro.local>
2018-04-09 08:00:51 -0500

    Uploaded app

---
+++ bamazon.sql
@@ -0,0 +1,60 @@
+-- Drops the animals_db if it exists currently --
+DROP DATABASE IF EXISTS bamazon;
+-- Creates the "animals_db" database --
+CREATE DATABASE bamazon;
+
+-- Makes it so all of the following code will affect animals_db --
+USE bamazon;
+
+-- Creates the table "products" within animals_db --
+CREATE TABLE products (
+  -- Creates a numeric column called "id" which will automatically increment its default value as we create new rows --
+  item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  product_name VARCHAR(255) NOT NULL,
+  dept_name VARCHAR(30),
+  price DECIMAL(10,2),
+  stock_qty INTEGER (6)
+  -- Sets id as this table's primary key which means all data contained within it will be unique --
+  PRIMARY KEY (id)
+);
+
+-- Creates new rows containing data in all named columns --
+INSERT INTO products (product_name, dept_name, price, stock_qty)
+VALUES ("SINGER 2277 Tradition Sewing Machine", "Arts, Crafts & Sewing", 107.10, 150);
+
+INSERT INTO products (product_name, dept_name, price, stock_qty)
+VALUES ("Moto G PLUS (5th Generation) - 64 GB - Unlocked", "Cell Phones", 259.99, 1021);
+
+INSERT INTO products (product_name, dept_name, price, stock_qty)
+VALUES ("Zevia—Ginger Beer—7.5 oz. Can (12 Count)—Zero Calories or Sugar, Naturally Sweetened with Stevia Leaf Extract", "Seltzer Water", 17.56, 670);
+
+INSERT INTO products (product_name, dept_name, price, stock_qty)
+VALUES ("Andantex R3000 Right Angle Bevel Gear Drive, Universal Mounting, Single Output Shaft, 2 Flanges, Inch, 3/8 in Shaft Diameter, 1:1 Ratio, .34Hp at 1750rpm", "Cell Phones", 147.06, 72);
+
+INSERT INTO products (product_name, dept_name, price, stock_qty)
+VALUES ("Bamboo Epoxy Stand Up Paddle Board PKG Red Rail by JK", "Stand-Up Paddleboards", 699.00, 49);
+
+INSERT INTO products (product_name, dept_name, price, stock_qty)
+VALUES ("Beyond Legacy Code: Nine Practices to Extend the Life (and Value) of Your Software", "Textbooks", 32.54, 358);
+
+INSERT INTO products (product_name, dept_name, price, stock_qty)
+VALUES ("Grokking Algorithms: An illustrated guide for programmers and other curious people", "Textbooks", 18.27, 1223);
+
+INSERT INTO products (product_name, dept_name, price, stock_qty)
+VALUES ("NASA Saturn V 1967-1973 (Apollo 4 to Apollo 17 & Skylab) (Owners' Workshop Manual)", "Textbooks", 28.54, 3100);
+
+INSERT INTO products (product_name, dept_name, price, stock_qty)
+VALUES ("Jitterbug Flip Easy-to-Use Cell Phone for Seniors - Red by GreatCall", "Cell Phones", 60.87, 983);
+
+INSERT INTO products (product_name, dept_name, price, stock_qty)
+VALUES ("TracFone LG L442BG 3G Prepaid Phone", "Cell Phones", 12.99, 0);
+
+INSERT INTO products (product_name, dept_name, price, stock_qty)
+VALUES ("Foundlings Organic Cotton Stuffed Bunny", "Stuffed Animals & Teddy Bears ", 26.50, 32);
+
+-- Updates the row --
+UPDATE products
+SET dept_name = 'Power Transmission'
+WHERE item_id = 4;
+
+SELECT * FROM bamazon.products;
---
+++ bamazonCustomer.js
@@ -0,0 +1,111 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "root",
+    database: "bamazon"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    showAll();
+});
+
+// first display all of the items available for sale. Include the ids, names, and prices of products for sale.
+
+function showAll() {
+    connection.query("SELECT * FROM products;", function (err, res) {
+        if (err) console.log(err);
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].dept_name + " | $" + res[i].price);
+        }
+        console.log("-----------------------------------");
+        orderSelect();
+    });
+};
+
+//   The app should then prompt users with two messages.
+//    * The first should ask them the ID of the product they would like to buy.
+//    * The second message should ask how many units of the product they would like to buy.
+
+function orderSelect() {
+    var questions = [
+        {
+            type: 'input',
+            name: 'chosenNum',
+            message: 'Which item would you like? Please enter its number:',
+            validate: function (value) {
+                var valid = !isNaN(parseFloat(value));
+                return valid || 'Please enter a number';
+            },
+            filter: Number
+        },
+        {
+            type: 'input',
+            name: 'amount',
+            message: 'How many do you need?',
+            validate: function (value) {
+                var valid = !isNaN(parseFloat(value));
+                return valid || 'Please enter a number';
+            },
+            filter: Number
+        },
+    ]
+
+    inquirer.prompt(questions).then(function (answer) {
+        connection.query("SELECT * FROM products;", function (err, results) {
+            if (err) console.log(err);
+            // get the information of the chosen item
+            var chosenItem;
+            for (var i = 0; i < results.length; i++) {
+                if (results[i].item_id === answer.chosenNum) {
+                    chosenItem = results[i];
+                }
+            }
+        })
+    });
+    // inquirer.prompt(questions).then(answers => {
+    //     console.log('\nOrder receipt:');
+    //     console.log(JSON.stringify(answers, null, '  '));
+    // })
+
+    // 7. Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+
+    function fillOrder() {
+        if (chosenItem.stock_qty >= parseInt(answer.amount)) {
+            //    8. However, if your store _does_ have enough of the product, you should fulfill the customer's order.
+            //    * This means updating the SQL database to reflect the remaining quantity.
+            //    * Once the update goes through, show the customer the total cost of their purchase.
+            // bid was high enough, so update db, let the user know, and start over
+            connection.query(
+                "UPDATE products SET ? WHERE ?",
+                [
+                    {
+                        stock_qty: stock_qty - 1
+                    },
+                    {
+                        item_id: chosenItem.item_id
+                    }
+                ],
+                function (error) {
+                    if (error) throw err;
+                    console.log("Bid placed successfully!");
+                    start();
+                }
+            )
+        }
+        else {
+            //    * If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
+            // bid wasn't high enough, so apologize and start over
+            console.log("Insufficient stock available. Please try again...");
+            showAll();
+        }
+    }
+};
